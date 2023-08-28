65c791e09265cc6b32035a541be1796f0fb2b94d
Muhammad Rohman Irfanuddin <rirfanuddin.m@gmail.com>
2023-08-28 15:34:06 +0800

    add table peminjaman_buku and generate with php artisan migrate --path='./database/migrations/2023_08_28_041335_create_peminjaman_buku_table.php'

---
+++ database/migrations/2023_08_28_041335_create_peminjaman_buku_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peminjaman_buku', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('book_id');
+            $table->foreign('book_id')->references('id')->on('gorlib_buku');
+            $table->date('tanggal_harus_kembali');
+            $table->string('status');
+            $table->dateTime('tanggal_kembali');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peminjaman_buku');
+    }
+};
